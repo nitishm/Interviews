3252880704e65abeb852a7092043bb25ffe1201f
Nitish Malhotra <nitish.malhotra@gmail.com>
2016-07-25 18:43:50 -0400

    lala

--- btTree.py
+++ btTree.py
@@ -1,3 +1,5 @@
+import math
+
 class Node:
     def __init__(self, value, parent = None):
         self.value = value
@@ -101,17 +103,69 @@ class BTree:
                     node.value = node.right.value
                     node.right = None
 
+    def depth(self, root):
+        if root == None:
+            return 0
+        return 1 + max(self.depth(root.left), self.depth(root.right))
 
+    def isBalanced(self, root):
+        if abs(self.depth(root.left) - self.depth(root.right)) > 1 and self.isBalanced(root.left) and self.isBalanced(root.right):
+            return False
+        return True
 
-t1 = BTree()
-l = [15,3,2,1,66,10,4,9,17,67,13]
-for item in l:
-    t1.insert(t1.root , item)
-t1.inOrder(t1.root)
-print "details"
-print t1.root.value
-print t1.findMin(t1.root).value
-print t1.findSuccessor(t1.root).value
-t1.delete(10)
-t1.delete(66)
-t1.inOrder(t1.root)
+    def isBalancedOptimalHelper(self, root):
+        if root == None:
+            return 0
+
+        heightLeft = self.isBalancedOptimalHelper(root.left)
+        if heightLeft == -1:
+            return -1
+        heightRight = self.isBalancedOptimalHelper(root.right)
+        if heightRight == -1:
+            return -1
+
+        if abs(heightLeft - heightRight) > 1:
+            return -1
+        else:
+            return 1 + max(heightRight, heightLeft)
+
+    def isBalancedOptimal(self, root):
+        if self.isBalancedOptimalHelper(root) == -1:
+            return False
+        else:
+            return True
+
+def BTreeFromSortedArray(arr, t2):
+    if len(arr) > 2:
+        pivotVal = len(arr)//2
+        pivot = arr[pivotVal]
+        t2.insert(t2.root, pivot)
+        print arr[pivotVal]
+        BTreeFromSortedArray(arr[0:pivotVal], t2)
+        BTreeFromSortedArray(arr[pivotVal:], t2)
+
+    return t2
+
+# t1 = BTree()
+# l = [15,3,2,1,66,10,4,9,17,67,13, 54]
+# for item in l:
+#     t1.insert(t1.root , item)
+# t1.inOrder(t1.root)
+# print "details"
+# print t1.root.value
+# print t1.findMin(t1.root).value
+# print t1.findSuccessor(t1.root).value
+# # t1.delete(10)
+# # t1.delete(66)
+# t1.inOrder(t1.root)
+# print t1.depth(t1.root.left)
+# print t1.depth(t1.root.right)
+
+# print t1.isBalanced(t1.root)
+# print t1.isBalancedOptimal(t1.root)
+
+t2 = BTree()
+arr = [i for i in range(1,20)]
+
+t2 = BTreeFromSortedArray(arr, t2)
+print t2.isBalanced(t2.root)
